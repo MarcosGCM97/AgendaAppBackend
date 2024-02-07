6281431b190490983305a407afc3bd784c964bb9
Marcos Congregado <marcoscongregado@gmail.com>
2024-02-07 17:41:19 -0300

    add mongoDB commit

---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length<3){
+    console.log('give password as argument');
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+    `mongodb+srv://marcoscongregado:${password}@cluster0.4h79rpg.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personShema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personShema)
+
+const person = new Person({
+    name: 'Robert',
+    number: '6586-324-5123'
+})
+
+person.save().then(result => {
+    console.log('person saved!', result)
+    mongoose.connection.close()
+})
